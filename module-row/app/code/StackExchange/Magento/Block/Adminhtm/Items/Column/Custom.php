020c59eeda7861ee131fd5aaeb6cc91442f02e27
devamitbera <dev.amitbera@gmail.com>
2019-09-16 17:43:27 +0530

    remove understand

--- module-row/app/code/StackExchange/Magento/Block/Adminhtm/Items/Column/Custom.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace StackExchange\Magento\Block\Adminhtm\Items\Column;
-
-
-class Custom  extends \Magento\Sales\Block\Adminhtml\Items\Column\DefaultColumn
-{
-    
-}
--- module-row/app/code/StackExchange/Magento/registration.php
+++
@@ -1,6 +0,0 @@
-<?php
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'StackExchange_Magento',
-    __DIR__
-);
