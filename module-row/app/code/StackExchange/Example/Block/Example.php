fcc3b20a8e2d7a540fbb658a4ddd4731d9807eae
devamitbera <dev.amitbera@gmail.com>
2019-09-16 19:02:50 +0530

    Add A blok & phtml to Layout file

--- module-row/app/code/StackExchange/Example/Block/Example.php
+++ module-row/app/code/StackExchange/Example/Block/Example.php
@@ -1,13 +1,9 @@
 <?php
-
-
 namespace StackExchange\Example\Block;
 
 
 class Example extends \Magento\Framework\View\Element\Template
 {
-    public function _toHtml() {
-        return __METHOD__;
-    }
+   
     
 }
